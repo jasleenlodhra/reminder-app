beb3dbb9fe47691cc19f46cfa8b8112c1ef5b133
Mariah Mirzayan <mariahm@DESKTOP-4974PLS.hitronhub.home>
2020-11-20 15:02:37 -0800

    create page html edited to include tags form

--- database.js
+++ database.js
@@ -1,6 +1,12 @@
 let Database = {
     cindy: {
-        reminders: []
+        reminders: [{id: 1, title: "abc", description: "abcabc", completed: false, subtasks: [
+            {
+                title: "subtask 1",
+                description: "sub desc"
+            }
+
+        ], tags: ["food", "shopping"]}]
     },
     alex: {
         reminders: []
