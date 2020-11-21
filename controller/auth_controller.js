292455f8945de886d8719ec7d010f0116dd96860
sajjin <sajjinnijjar@gmail.com>
2020-11-20 17:50:06 -0800

    login and register functions

--- controller/auth_controller.js
+++ controller/auth_controller.js
@@ -2,8 +2,8 @@ let database = require("../database");
 
 let authController = {
     login: (req, res) => {
-      res.locals.page = "login"
-      res.render('auth/login')
+        res.locals.page = "login"
+        res.render('auth/login')
     },
 
     register: (req, res) => {
@@ -12,11 +12,31 @@ let authController = {
     },
 
     loginSubmit: (req, res) => {
-        // implement
+        console.log(req.body.username)
+        console.log(req.body)
+        console.log(database[req.body.username])
+        if (database[req.body.username] && database[req.body.username].password === req.body.password) {
+           req.session['user'] = req.body.username;
+           res.redirect("/reminders")
+       } else {
+           res.send("incorrect username or password")
+       }
     },
 
     registerSubmit: (req, res) => {
-        // implement
+        if (req.body.username && req.body.password) {
+            database[req.body.username] = {username: req.body.username, password: req.body.password, tags: null, reminders: []}
+
+            console.log(database)
+            console.log(req.body.username)
+
+            req.session['user'] = req.body.username;
+            res.redirect("/login")
+        } else {
+            res.status(400)
+            res.send("invalid user")
+        }
+
     }
 }
 
