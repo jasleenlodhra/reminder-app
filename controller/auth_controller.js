c08928636e5a528fc54a769f5072a1b9a4be1799
sajjin <sajjinnijjar@gmail.com>
2020-11-26 12:45:30 -0800

    Fixed login feature and dynamic reminders

--- controller/auth_controller.js
+++ controller/auth_controller.js
@@ -2,19 +2,39 @@ let database = require("../database");
 
 let authController = {
     login: (req, res) => {
+        res.locals.page = "login"
         res.render('auth/login')
     },
 
     register: (req, res) => {
+        res.locals.page = "register"
         res.render('auth/register')
     },
 
     loginSubmit: (req, res) => {
-        // implement
+        let username = req.body.username.split("@")[0]
+        if (database[username] && database[username].password === req.body.password) {
+            req.session['user'] = username;
+            res.redirect("/reminders")
+        } else {
+            res.send("incorrect username or password")
+        }
     },
 
     registerSubmit: (req, res) => {
-        // implement
+        let username = req.body.username.split("@")[0]
+        if (req.body.username && req.body.password) {
+            database[username] = {username: username, password: req.body.password, reminders: []}
+
+            console.log(database)
+
+            req.session['user'] = username;
+            res.redirect("/reminders")
+        } else {
+            res.status(400)
+            res.send("invalid user")
+        }
+
     }
 }
 
--- controller/reminder_controller.js
+++ controller/reminder_controller.js
@@ -3,81 +3,114 @@ let database = require("../database");
 let remindersController = {
   // Show a list of reminders
   list: (req, res) => {
-    res.render('reminder/index', { reminders: database.cindy.reminders })
+    let username = req.session.user
+    res.render('reminder/index', {reminders: database[username].reminders})
+  },
+
+  // Add a Friends Page
+  addFriendsPage: (req, res) => {
+    let allUsers = Object.keys(database)
+    res.render("reminder/friends", {allUsers});
+  },
+
+  // Add Friend
+  addFriend: (req, res) => {
+    let username = req.session.user
+    console.log(req.query);
+    let friend = req.query.username;
+    console.log(friend);
+    // Insert into currently logged user's friends array
+    database[username].friends.push(friend);
+    console.log(database) // noice
+    res.render("reminder/addFriend", {friendList: database[username].friends})
+    // res.redirect('/reminder/' + reminderToFind)
   },
 
   // Show a Create Reminder Page
   new: (req, res) => {
-    res.render('reminder/create')
+    res.locals.page = 'create';
+    res.render('reminder/create');
   },
 
   // Show the details of a Single Reminder
   listOne: (req, res) => {
+    let username = req.session.user
     let reminderToFind = req.params.id;
-    let searchResult = database.cindy.reminders.find(function (reminder) {
+    let searchResult = database[username].reminders.find(function (reminder) {
       return reminder.id == reminderToFind;
     })
     if (searchResult !== undefined) {
-      res.render('reminder/single-reminder', { reminderItem: searchResult })
+      res.render('reminder/single-reminder', {reminderItem: searchResult})
     } else {
-      res.render('reminder/index', { reminders: database.cindy.reminders })
+      res.render('reminder/index', {reminders: database[username].reminders})
     }
   },
 
   // Create a reminder
-  // ⚠️ TODO: Currently hardcoded to always create a reminder for Cindy only. You need to make this dynamic. 
   create: (req, res) => {
+    let username = req.session.user
+
     let reminder = {
-      id: database.cindy.reminders.length + 1,
+      id: database[username].reminders.length + 1,
       title: req.body.title,
       description: req.body.description,
-      completed: 'false'
+      tags: req.body.tags,
+      completed: false
     }
-    database.cindy.reminders.push(reminder);
+    database[username].reminders.push(reminder);
+    res.locals.page = 'reminder list'
     res.redirect('/reminders');
   },
 
   // Show the Edit Reminder Page
   edit: (req, res) => {
     // ⭐️ your implementation here ⭐️
+    let username = req.session.user
+
     let reminderToFind = req.params.id;
-    let searchResult = database.cindy.reminders.find(function (reminder) {
+    let searchResult = database[username].reminders.find(function (reminder) {
       return reminder.id == reminderToFind;
     })
-    res.render('reminder/edit', { reminderItem: searchResult })
+    res.render('reminder/edit', {reminderItem: searchResult})
   },
 
   // Edit the Reminder
   update: (req, res) => {
-    // ⭐️ your implementation here ⭐️
+    // ⭐️ your implementation here ⭐
+    let username = req.session.user
     let reminderToFind = req.params.id;
-    let searchResult = database.cindy.reminders.find(function (reminder) {
+    let searchResult = database[username].reminders.find(function (reminder) {
       return reminder.id == reminderToFind;
     })
-    let removeIndex = database.cindy.reminders.map(function(item) { return item.id; }).indexOf(searchResult.id);
-    database.cindy.reminders.splice(removeIndex, 1)
+    let removeIndex = database[username].reminders.map(function (item) {
+      return item.id;
+    }).indexOf(searchResult.id);
+    database[username].reminders.splice(removeIndex, 1)
     let reminder = {
-      id: database.cindy.reminders.length,
+      id: database[username].reminders.length,
       title: req.body.title,
       description: req.body.description,
-      completed: req.body.completed
+      tags: req.body.tags.split(","),
+      completed: req.body.completed == "true"
     }
-    console.log(reminder)
-    database.cindy.reminders.push(reminder);
-    console.log(database.cindy.reminders)
+    database[username].reminders.push(reminder);
     res.redirect('/reminders');
   },
 
   // Delete the Reminder
   delete: (req, res) => {
     // ⭐️ your implementation here ⭐️
-    let reminderToDelete = req.params.id;
-    for (let i=0; i<database.cindy.reminders.length; i++){
-      if (database.cindy.reminders[i].id == reminderToDelete){
-        database.cindy.reminders.splice(i,1)
-      }
-    }
-    res.redirect('/reminders')
+    let username = req.session.user
+    let reminderToFind = req.params.id;
+    let searchResult = database[username].reminders.find(function (reminder) {
+      return reminder.id == reminderToFind;
+    })
+    let removeIndex = database[username].reminders.map(function (item) {
+      return item.id;
+    }).indexOf(searchResult.id);
+    database[username].reminders.splice(removeIndex, 1)
+    res.redirect('/reminders');
   }
 }
+
 module.exports = remindersController;
